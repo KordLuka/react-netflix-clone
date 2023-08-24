3e902310c49b539e7734265943f591eb18d80ff9
Kordian <kordianlukask@gmail.com>
2023-08-24 19:14:18 +0200

    Added the navbar component

--- netflix-clone/pages/index.tsx
+++ netflix-clone/pages/index.tsx
@@ -1,34 +1,28 @@
-import { signOut, getSession } from "next-auth/react";
-import { NextPageContext } from "next";
-import useCurrentUser from "@/hooks/useCurrentUser";
+import { getSession } from 'next-auth/react'
+import { NextPageContext } from 'next'
+import Navbar from '@/components/Navbar'
 
 export async function getServerSideProps(context: NextPageContext) {
-  const session = await getSession(context);
+    const session = await getSession(context)
 
-  if (!session) {
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/auth',
+                permanent: false,
+            },
+        }
+    }
+
     return {
-      redirect: {
-        destination: "/auth",
-        permanent: false,
-      },
-    };
-  }
-
-  return {
-    props: {},
-  };
+        props: {},
+    }
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
-
-  return (
-    <div>
-      <h1 className="text-4xl text-green-500">Netflix Clone</h1>
-      <p className="text-white">Logged in as: {user?.name}</p>
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
-        Logout!
-      </button>
-    </div>
-  );
+    return (
+        <>
+            <Navbar />
+        </>
+    )
 }
